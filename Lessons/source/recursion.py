2d15fe860ecb148ed7a53bc71cc9a4a68812ab00
Egon Fiedler <efiedler93@gmail.com>
2019-04-03 14:07:23 -0700

    refactored some code

--- Lessons/source/recursion.py
+++ Lessons/source/recursion.py
@@ -9,21 +9,33 @@ def factorial(n):
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
     # return factorial_iterative(n)
-    return factorial_recursive(n)
+    return factorial_recursive1(n)
 
 
 def factorial_iterative(n):
     # TODO: implement the factorial function iteratively here
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests
-    a = 1
-    k = 1
-    while a < n: #a is less than n
-        #Estoy restando el valor de n hasta q sea
-        k = k * n
-        n = n - a
 
-    return k
+    # This two variables 
+    dummy_one = 1
+    final_product = 1
+    while dummy_one < n: #a is less than n
+        final_product = final_product * n
+        n = n - dummy_one
+
+    return final_product
+
+def factorial_iterative1(n):
+
+# n is the input we get, we have to make it become the solution
+    final_product = 1
+    while n > 1:
+        final_product *= n
+        n -= 1
+
+    return final_product
+
 
 def factorial_recursive(n):
     # check if n is one of the base cases
@@ -34,6 +46,21 @@ def factorial_recursive(n):
         # call function recursively
         return n * factorial_recursive(n - 1)
 
+def factorial_recursive(n):
+    # check if n is one of the base cases
+    if n == 0 or n == 1:
+        return 1
+    # check if n is an integer larger than the base cases
+    elif n > 1:
+        # call function recursively
+        return n * factorial_recursive(n - 1)
+
+def factorial_recursive1(n):
+    # check if n is an integer larger than the base cases
+    if n > 1:
+        # call function recursively
+        return n * factorial_recursive(n - 1)
+    return 1
 
 def main():
     import sys
