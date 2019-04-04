3eef356732d55d30e94c3fe046e477a25912d557
Egon Fiedler <efiedler93@gmail.com>
2019-04-03 23:20:58 -0700

    Finish base challenges, time to refactor

--- Lessons/source/search.py
+++ Lessons/source/search.py
@@ -26,21 +26,59 @@ def linear_search_recursive(array, item, index=0):
 
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
-    # implement binary_search_iterative and binary_search_recursive below, then
-    # change this to call your implementation to verify it passes all tests
-    return binary_search_iterative(array, item)
-    # return binary_search_recursive(array, item)
+    return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
-    # TODO: implement binary search iteratively here
-    pass
-    # once implemented, change binary_search to call binary_search_iterative
-    # to verify that your iterative implementation passes all tests
+    first_index = 0
+    last_index = len(array)-1
+    #this will iterate until it becomes continuous
+    if item == array[first_index]:
+        return first_index
+    if item == array[last_index]:
+        return last_index
 
+    while first_index <= last_index:
+        half = int((last_index + first_index) / 2)
+        # print('half: {}'.format(half))
+         #half point in the array
+        if item == array[half]: # item is equal to element in position half at the array
+            return half         # if it is return the position
+        #This is comparing a string against a list
+        if item > array[half]:
+            first_index = half + 1  # ignore left half
+
+        #it must check smaller than it
+        if item < array[half]:  
+            last_index = half - 1  # ignore right half
+    # print('while loop ended. first: {}, last: {}'.format(first_index, last_index))
+    
+   
 
 def binary_search_recursive(array, item, left=None, right=None):
-    # TODO: implement binary search recursively here
-    pass
-    # once implemented, change binary_search to call binary_search_recursive
-    # to verify that your recursive implementation passes all tests
+    # Recursive is all about calling the function inside the function
+    # Binary search is all about dividing base on the position of the item in the array
+    if left == None:
+        left = 0
+    if right == None:
+        right = len(array)-1
+
+    half = int((left + right) / 2)
+
+    if left == right:
+        return None
+
+    if item == array[half]:
+        return half
+
+    if item == array[right]:
+        return right
+    if item > array[half]:
+        return binary_search_recursive(array, item, half + 1, right)
+
+    if item == array[left]:
+        return left
+    if item < array[half]:
+        return binary_search_recursive(array, item, left, half - 1)
+    
+
