4def1674af4a9230db0ff5042260cf3c1bedd46c
Egon Fiedler <efiedler93@gmail.com>
2019-04-18 20:20:04 -0700

    finishing the ArrayStack

--- Lessons/source/stack.py
+++ Lessons/source/stack.py
@@ -48,7 +48,12 @@ class LinkedStack(object):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        if not self.list.is_empty():
+            item = self.list.head.data
+            self.list.delete(item)
+            return item
+        else:
+            raise ValueError('The stack is empty')
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
@@ -69,16 +74,19 @@ class ArrayStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        if len(self.list) == 0:
+            return True
+        else:
+            return False
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        return len(self.list)
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        self.list.append(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
