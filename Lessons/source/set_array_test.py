48a1ca8e1a3af0f0d5332fda7136b29d1fd6927e
Egon Fiedler <efiedler93@gmail.com>
2019-04-28 22:56:22 -0700

    not done yet, still must run

--- Lessons/source/set_array_test.py
+++ Lessons/source/set_array_test.py
@@ -8,4 +8,4 @@ class SetTest(unittest.TestCase):
     def test_init(self):
         int_item = 123
         set_array = Set(int_item)
-        assert set_array.dynamic_array is int_item
+        assert set_array.members == [int_item]
