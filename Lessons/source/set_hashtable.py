26658b7470ac6bd1808f6422e039f289e820119e
Egon Fiedler <efiedler93@gmail.com>
2019-05-17 09:58:15 -0700

    union DONE, intersection DONE

--- Lessons/source/set_hashtable.py
+++ Lessons/source/set_hashtable.py
@@ -14,9 +14,9 @@ from linkedlist import LinkedList, Node
 
 class SetHashTable(object):
 
-    def __init__(self, data=None):
+    def __init__(self, init_size=None):
         self.hashtable = HashTable()
-        self.hashtable.buckets = [LinkedList() for i in range(5)]
+        self.hashtable.buckets = [LinkedList() for i in range(init_size)]
         self.hashtable.size = 0
     
     def size(self):
@@ -29,16 +29,26 @@ class SetHashTable(object):
         return self.hashtable.contains(key)
 
     def insert(self, key, value):
-        if self.is_member(key) is not True:
-            self.hashtable.set(key, value)
-        raise ValueError('Item is already a member: {}'.format(item))
+        if self.is_member(key) is True:
+            raise ValueError('Item is already a member: {}'.format(key))
+        self.hashtable.set(key, value)
+        
     
     def union(self, other_set):
-        pass
-
+        new_set = SetHashTable()
+        for index, member in self.all_members():
+            new_set.insert(index, member)
+        for index, member in other_set.all_members():
+            new_set.insert(index, member)
+        return new_set
 
     def intersection(self, other_set):
-        pass
+        # Must get both elements that intercept here
+        new_set = SetHashTable()
+        for key, member in self.all_members():
+            if other_set.is_member(key, member):
+                new_set.insert(key, member)
+        return new_set
 
     def subset(self, other_set):
         pass
