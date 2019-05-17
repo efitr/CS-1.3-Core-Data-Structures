30c81ad286348dfd1bac38050ab75f23854f608c
Egon Fiedler <efiedler93@gmail.com>
2019-05-17 05:47:45 -0700

    class SetHashTable made with everything it must have

---
+++ Lessons/source/set_hashtable.py
@@ -0,0 +1,45 @@
+from hashtable import HashTable
+from linkedlist import LinkedList, Node
+# Set basic properties
+    # * Size
+# Set basic operations
+    # * Insert
+    # * Contains
+    # * Remove
+# Set properties operation
+    # * Union
+    # * Intersection
+    # * Subset
+    # * Symetric Difference
+
+class SetHashTable(object):
+
+    def __init__(self, data=None):
+        pass
+    
+    def size(self):
+        pass
+    
+    def contains(self, item):
+        pass
+
+    def insert(self, item):
+        pass
+    
+    def delete(self, member):
+        pass
+    
+    def intersection(self, other_set):
+        pass
+
+    def union(self, other_set):
+        pass
+
+    def subset(self, other_set):
+        pass
+
+    def symetric_difference(self, other_set):
+        pass
+        
+    
+    
