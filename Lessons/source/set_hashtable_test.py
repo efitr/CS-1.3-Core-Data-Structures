ea69d41d5589c8f7fc1628f4c62d5e5ea1aa230c
Egon Fiedler <efiedler93@gmail.com>
2019-05-17 09:32:15 -0700

    All tests are passing

--- Lessons/source/set_hashtable_test.py
+++ Lessons/source/set_hashtable_test.py
@@ -9,41 +9,28 @@ if not hasattr(unittest.TestCase, 'assertCountEqual'):
 class HashTableTest(unittest.TestCase):
 
     def test_init(self):
-        set_ht = SetHashTable(4)
+        set_ht = SetHashTable(5)
         # set_hashtable = Se
         assert len(set_ht.hashtable.buckets) == 5
         assert set_ht.size() == 0
-        # assert set_ht.size == 0
 
-    # def test_all_members(self):
-    #     ht = HashTable()
-    #     assert ht.items() == []
-    #     ht.set('I', 1)
-    #     assert ht.items() == [('I', 1)]
-    #     ht.set('V', 5)
-    #     self.assertCountEqual(ht.items(), [('I', 1), ('V', 5)])
-    #     ht.set('X', 10)
-    #     self.assertCountEqual(ht.items(), [('I', 1), ('V', 5), ('X', 10)])
+    def test_all_members(self):
+        set_ht = SetHashTable(5)
+        assert set_ht.all_members() == []
+        set_ht.insert('I', 1)
+        assert set_ht.all_members() == [('I', 1)]
+        set_ht.insert('V', 5)
+        self.assertCountEqual(set_ht.all_members(), [('I', 1), ('V', 5)])
 
-    # def test_length(self):
-    #     ht = HashTable()
-    #     assert ht.length() == 0
-    #     ht.set('I', 1)
-    #     assert ht.length() == 1
-    #     ht.set('V', 5)
-    #     assert ht.length() == 2
-    #     ht.set('X', 10)
-    #     assert ht.length() == 3
-
-    # def test_size(self):
-    #     ht = HashTable()
-    #     assert ht.size == 0
-    #     ht.set('I', 1)
-    #     assert ht.size == 1
-    #     ht.set('V', 5)
-    #     assert ht.size == 2
-    #     ht.set('X', 10)
-    #     assert ht.size == 3
+    def test_is_member(self):
+        set_ht = SetHashTable(5)
+        set_ht.insert('T', 1)
+        set_ht.insert('Y', 5)
+        set_ht.insert('J', 10)
+        assert set_ht.is_member('T') is True
+        assert set_ht.is_member('Y') is True
+        assert set_ht.is_member('J') is True
+        assert set_ht.is_member('A') is False
 
 if __name__ == '__main__':
     unittest.main()
