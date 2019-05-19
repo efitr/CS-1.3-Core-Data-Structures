657c4e26c5161e5acd988a0d1d542f92d3bcc59c
Egon Fiedler <efiedler93@gmail.com>
2019-05-19 02:55:11 -0700

    Made a Big O analysis

--- Lessons/source/strings.py
+++ Lessons/source/strings.py
@@ -1,7 +1,6 @@
 #!python
 
-# make it be recursive and iterative
-#   The Recursive handles the index, 
+import timeit
 
 def contains(text, pattern):
     return iterative_contains(text, pattern)
@@ -19,8 +18,13 @@ def iterative_contains(text, pattern):
         return False
     return True
 
-# def recursive_contains(text, pattern, text_index, pattern_index):
-def recursive_contains(text, pattern, text_index = 0, pattern_index = 0):
+def recursive_contains(text, pattern, text_index, pattern_index):
+    ''' 
+    Best case running time: O(pattern_n) under what conditions, if the item is at the start 
+    Worst case running time: O(n + n(pattern_n-1)) under what conditions, if the pattern is at the end 
+    and there are many almost patters
+    '''
+# def recursive_contains(text, pattern, text_index = 0, pattern_index = 0):
     if len(text) <= len(pattern):
     # If they are the same lenght and equal, it's true
         if text is pattern:
@@ -34,16 +38,14 @@ def recursive_contains(text, pattern, text_index = 0, pattern_index = 0):
     if pattern_index is len(pattern) - 1:
         return True
 
-    if text_index is len(text) - 1 and text[len(text) - 1] is not pattern[0]:
-        return False
-    
-        
     # This is checking for every point in the 
     if text[text_index] is pattern[pattern_index]:
         # if len(pattern)
         return recursive_contains(text, pattern, text_index + 1, pattern_index + 1)
 
     if text[text_index] is not pattern[pattern_index]:
+        if text_index is len(text) - 1 and text[len(text) - 1] is not pattern[0]:
+            return False
         return recursive_contains(text, pattern, text_index + 1, 0)
 
 
@@ -53,11 +55,12 @@ def find_index(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_index here (iteratively and/or recursively)
-    # This handles the edge cases
-    # if contains(text, pattern) is False:
-    #     return None
-    # This is more efficient, because does less operation, while the code in contains and find_index 
-    # is almost the same
+    ''' 
+    Best case running time: O(pattern_n) under what conditions, if the item is at the start 
+    Worst case running time: O(n + n(pattern_n-1) + n(pattern_n)) under what conditions, if the pattern is at the end 
+    and there are many almost patters? 
+    '''
+
     if len(text) <= len(pattern):
         # If they are the same lenght and equal, it's true
         if text is pattern:
@@ -90,7 +93,11 @@ def find_all_indexes(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_all_indexes here (iteratively and/or recursively)
-    
+    ''' 
+    Best case running time: O(pattern_n) under what conditions, if the item is at the start? 
+    Worst case running time: O(n + n(pattern_n) + n(pattern_n)) under what conditions, if the pattern is at the end 
+    and there are many almost patters? 
+    '''
     all_indexes = []
 
     if len(text) <= len(pattern):
