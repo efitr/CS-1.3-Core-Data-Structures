1597b465d600ef1f820450e73e30e0fd038f8a3a
Egon Fiedler <efiedler93@gmail.com>
2019-05-19 17:30:23 -0700

    Intersection works

--- Lessons/source/set_hashtable.py
+++ Lessons/source/set_hashtable.py
@@ -17,9 +17,9 @@ class SetHashTable(object):
     def __init__(self, init_size=None):
         self.hashtable = HashTable()
         self.hashtable.buckets = [LinkedList() for i in range(init_size)]
-        self.hashtable.size = 0
+        self.size = 0
     
-    def size(self):
+    def length(self):
         return self.hashtable.length()
         
     def all_members(self):
@@ -32,25 +32,34 @@ class SetHashTable(object):
         if self.is_member(key) is True:
             raise ValueError('Item is already a member: {}'.format(key))
         self.hashtable.set(key, value)
+        self.size += 1
     
     def remove(self, key):
         if self.is_member(key) is False:
             raise ValueError('Item is not a member: {}'.format(key))
+        self.size -= 1
         return self.hashtable.delete(key)
     
-    def union(self, other_set):
-        new_set = SetHashTable()
-        for index, member in self.all_members():
+    def union(self, some_set, other_set):
+        new_set = SetHashTable(5)
+        for index, member in some_set.all_members():
             new_set.insert(index, member)
         for index, member in other_set.all_members():
             new_set.insert(index, member)
         return new_set
 
-    def intersection(self, other_set):
+    def intersection(self, some_set, other_set):
         # Must get both elements that intercept here
-        new_set = SetHashTable()
-        for key, member in self.all_members():
-            if other_set.is_member(key, member):
+        new_set = SetHashTable(5)
+        # Optimization for when some_set is longer or equally long
+        if some_set.length() >= other_set.length():
+            for key, member in some_set.all_members():
+                if other_set.is_member(key):
+                    new_set.insert(key, member)
+            return new_set
+        # Optimization for when other set is longer
+        for key, member in other_set.all_members():
+            if some_set.is_member(key):
                 new_set.insert(key, member)
         return new_set
 
